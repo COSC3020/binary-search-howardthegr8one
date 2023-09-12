bab24a368bca8b8cba7ab4fb6a56c6eb6e885961
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-09-12 12:05:04 -0600

    Should pass test now code.js
    had a +1 on a slice index that shouldn't have been there.

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function binarySearch(list, element) {
-    return -1;
+
+    if ( (target < list[0]) || (target > list[list.length-1]) )
+        return -1
+
+    let half = Math.floor(list.length / 2)
+
+    if (target == list[half])
+        return half
+    else if (target < list[half])
+        return binarySearch(list.slice(0, half), target)
+    else
+        return half + binarySearch(list.slice(half, list.length), target)
 }
