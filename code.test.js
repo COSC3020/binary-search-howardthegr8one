b85b46c3a84a0af63988f2194d958eb4f4e9847d
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-12-06 13:55:46 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -7,7 +7,7 @@ const testSearch =
     jsc.forall("array nat", function(arr) {
         if(arr.length > 0) {
             arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0;
+            return binarySearch(arr, arr[0]) === 1;
         } else {
             return binarySearch(arr, "foo") === -1;
         }
