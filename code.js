76ce1719fdd057b178e354f37f49bcb780b8e6a9
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-12-11 11:18:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,16 +2,19 @@ function binarySearch(list, element) {
 
     if (list.length < 1)
         return -1
+    else if (list.length == 1)
+        return 0
 
     if ( (element < list[0]) || (element > list[list.length-1]) )
         return -1
 
     let half = Math.floor(list.length / 2)
 
-    if (element == list[half])
-        return half
-    else if (element < list[half])
-        return binarySearch(list.slice(0, half), element)
+    if (element <= list[half])
+        if (element == list[half-1])
+            return binarySearch(list.slice(0, half), element)
+        else
+            return half
     else
         return half + binarySearch(list.slice(half, list.length), element)
 }
